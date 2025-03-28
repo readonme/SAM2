2f7d0f799b6811eac3532ffbc9d323d5ddeee679
root <root@localhost.localdomain>
2025-03-28 15:35:34 +0800

    优化 av 版本

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ EXTRA_PACKAGES = {
     "interactive-demo": [
         "Flask>=3.0.3",
         "Flask-Cors>=5.0.0",
-        "av>=13.0.0",
+        "av=13.1.0",
         "dataclasses-json>=0.6.7",
         "eva-decord>=0.6.1",
         "gunicorn>=23.0.0",
